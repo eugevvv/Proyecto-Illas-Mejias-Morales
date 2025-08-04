645014d338be925ae9a3a384d1b86d83e628745e
ana-maria-morales <ana.morales@correo.unimet.edu.ve>
2025-08-04 14:03:47 -0400

    Update try_api.py

--- try_api.py
+++ try_api.py
@@ -1,6 +1,17 @@
 import requests
 
-def datos_departamentos():
-    response = requests.get('https://collectionapi.metmuseum.org/public/collection/v1/departments')
+class met_API:
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/"
+
+def datos_departamentos(self):
+    response = requests.get(f"{self.url}departments)
     return response.json()
+    
+def buscar_por_departamentos(self, id_departamento):
+   buscar_url = f"{self.url}
+     
+    
+def buscar_por_autor 
+
+
 
