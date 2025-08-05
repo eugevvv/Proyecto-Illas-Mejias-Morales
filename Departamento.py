b2f3af0e3c10c01e00f83ba2d02fa2dd8681a9d5
eugevvv <eugenia.mejias@correo.unimet.edu.ve>
2025-08-05 14:40:21 -0400

    Update Departamento.py
    Muchachas, agregue def __init__ y def show con Docstring incluidos, revisen que tal

--- Departamento.py
+++ Departamento.py
@@ -1,7 +1,22 @@
 class Departamento:
     """
     Definición de Clase: Departamento, sus atributos y métodos.
+    Representa un departamento y guarda información básica del mismo.
     """
-    def __init__(self, id, nombre):
-        self.id= id
-        self.nombre= nombre
+    def __init__(self, id_dpto, nombre_dpto):
+        """
+        Atributo principal de la clase Departamento
+
+        Args:
+            id_dpto (int): ID numérico del departamento.
+            nombre_dpto (str): Nombre del departamento.
+        """
+        self.id_dpto= id_dpto
+        self.nombre_dpto= nombre_dpto
+
+    def show(self):
+        """
+        Muestra la información básica del departamento
+        """
+        print(f"{self.id.dpto} - {self.nombre_dpto})
+
