fda8afb01f5cad1ab1bebed665e7dbdd558b01ab
Wuiliany <illas.wuiliany@correo.unimet.edu.ve>
2025-08-05 23:38:22 -0400

    Update try_api.py
    niñas, ya están los datos obtenidos de la api,  cualquier cosa revísenlo y si llega a fallar lo arreglo, recuerden que para que corra deben tener las librerias requests y pillow instaladas, descansen :)

--- try_api.py
+++ try_api.py
@@ -1,17 +1,63 @@
+#Este apartado cumple con importar sus funciones al Catalogo
 import requests
+api = "https://collectionapi.metmuseum.org/public/collection/v1/"
 
-class met_API:
-    url = "https://collectionapi.metmuseum.org/public/collection/v1/"
-
-def datos_departamentos(self):
-    response = requests.get(f"{self.url}departments)
-    return response.json()
-    
-def buscar_por_departamentos(self, id_departamento):
-   buscar_url = f"{self.url}
-     
-    
-def buscar_por_autor 
-
-
-
+def obtener_departamentos():
+    url = f"{api}/ departments"
+    try
+        response = requests.get(url)
+        if response.status_code == 200:
+            datos = response.json()
+            return datos.get("departments", []) #Devolver lista vacía si no se encuentran los departamentos.
+        else:
+            print(f"Error al contactar la API de departamentos...")
+            return [] #lista vacia
+    except Exception:
+        print(f"Error de conexión con la API: {Exception}")
+        return []
+def obtener_ID_departamento(id_dpto):
+    """
+    Obtiene los IDs de todas las obras para un departamento específico.
+    """
+    url = f"{api}/objects?departmentIds={id_dpto}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            datos = response.json()
+            return datos.get('objectIDs', [])
+        else:
+            print(f"Error al obtener obras del departamento...")
+            return []
+    except Exception:
+        print(f"Error de conexión con la API: {Exception}") 
+        return []
+def buscar_ID_termino(termino_busqueda):
+    """
+    Busca obras que coincidan con un término de forma general y efectiva.
+    """
+    url = f"{api}/search?q={termino_busqueda}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            datos = response.json()
+            return datos.get('objectIDs', [])
+        else:
+            print(f"Error en la búsqueda por término. Código: {response.status_code}")
+            return []
+    except Exception:
+        print(f"Error de conexión con la API: {Exception}")
+        return []
+def obtener_detalle_obra(id_obra): 
+    """
+    Obtiene los detalles completos de una única obra de arte por su ID.
+    """
+    url = f"{api}/objects/{id_obra}"
+    try: 
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+    except Exception:
+        print(f"Error de conexión al buscar obra {id_obra}: {Exception}")
+        return None
