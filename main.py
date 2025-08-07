49f7a813d4265c75d69b7413b817e95000c81129
Wuiliany <illas.wuiliany@correo.unimet.edu.ve>
2025-08-07 15:22:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ Inicialización del sistema
 from Catalogo import Catalogo
 
 def main():
+  """Función principal, en donde se obtiene la información desarrollada y por medio de la 
+    función antes definida para inicializar el sistema.
+  """
   catalogo = Catalogo()
   catalogo.iniciar_sistema()
 main()
+
